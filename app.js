8d11363463450efc3f8b1be859be82d7d43fc8f3
Akshay <akshay@LAPTOP-725LK5NP.localdomain>
2022-06-09 14:08:20 +0530

    Fix deplpy errors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 app = express();
 
-
+app.get('/', function(req,res) {
+    res.sendFile(__dirname+'/index.html');
+})
 app.listen(process.env.PORT || 3000, () => {
     console.log("Running");
 });
